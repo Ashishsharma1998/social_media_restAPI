5d602f7517783bb74df5dd371ce7825cea22cd25
Ashish sharma <sharma07101998@gmail.com>
2023-05-05 00:33:22 +0530

    added file uploading functionality in diskStorage using multer

--- index.js
+++ index.js
@@ -3,16 +3,40 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const multer = require("multer");
 const cors = require("cors");
+const path = require("path");
 const userApiRoutes = require("./routes/users");
 const authApiRoutes = require("./routes/auth");
 const postApiRoutes = require("./routes/post");
 const app = express();
 dotenv.config();
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    res.status(200).json("file successfully uploaded!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.use("/api/user", userApiRoutes);
 app.use("/api/auth", authApiRoutes);
 app.use("/api/posts", postApiRoutes);
